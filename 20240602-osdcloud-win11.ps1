400df162ee76f6b50645558dd58a02a0e572a81d
Michiel Dekker <33653504+chield@users.noreply.github.com>
2024-02-06 12:59:41 +0100

    Update 20240602-osdcloud-win11.ps1

--- 20240602-osdcloud-win11.ps1
+++ 20240602-osdcloud-win11.ps1
@@ -17,10 +17,6 @@ Import-Module OSD -Force
 Write-Host  -ForegroundColor Cyan "Start OSDCloud with Interstellar Parameters"
 Start-OSDCloud -OSLanguage en-us -OSBuild 23H2 -OSEdition Pro -OSActivation Retail -ZTI
 
-#Anything I want  can go right here and I can change it at any time since it is in the Cloud!!!!!
-Write-Host  -ForegroundColor Cyan "Starting OSDCloud PostAction ..."
-Write-Warning "I'm not sure of what to put here yet"
-
 #Restart from WinPE
 Write-Host  -ForegroundColor Cyan "Restarting in 20 seconds!"
 Start-Sleep -Seconds 20
